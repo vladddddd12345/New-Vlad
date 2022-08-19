d19569f424eff8b901edcce954932f64fc06df39
vladddddd12345 <vlad.karamushka@wesoftyou.com>
2022-08-19 15:14:49 +0300

    good

--- features/step-definitions/newtestcheck.ts
+++ features/step-definitions/newtestcheck.ts
@@ -10,6 +10,10 @@ import * as assert from "assert";
         const emailfunct = $('[name=username]')
         await (await emailfunct).setValue(email)
         await browser.pause(2000)
+        await (await emailfunct).click()
+        await browser.keys(['Command', 'a'])
+        await browser.keys('Delete')
+        await browser.pause(2000)
 
     });
     When(/^I input password(.+)$/, async (password:string) => {
